5e24e059a95c4075340de9f675230e4cd1382a16
Skoryk Dmytro <skorikdeveloper@gmail.com>
2023-07-21 18:48:49 +0200

    change text for command my_interests

--- commands/user/SetUserInterestsCommand.php
+++ commands/user/SetUserInterestsCommand.php
@@ -38,7 +38,7 @@ class SetUserInterestsCommand extends Command
             'chat_id' => $telegram_id,
         ];
 		
-		$options['text'] = __("What are your interests, hobbies and values that you want to share with other people? You can use any language. You can just list topics or even write an essay about who you really are. We will use this information in order to bring you closer to people sharing those values and interests. So, the more specific you are, the more support we can provide", $user['user']['language']);
+		$options['text'] = __("Just imagine, all your pressing tasks are sorted out. You have more than enough money for any whim, and you enjoy complete harmony with your loved ones, knowing that everything is going well for them too. Your living conditions are exactly as you desire. Now, you have plenty of free time to spend on yourself. List them out (you can use commas or simply write them in a column): What activities would you engage in? Essentially, what actions would bring you joy and fulfilment (because let's face it, you wouldn't want to do things you don't enjoy)?", $user['user']['language']);
 		
 		$this->telegram->sendMessage($options);
 		set_command_to_last_message($this->name, $telegram_id);
