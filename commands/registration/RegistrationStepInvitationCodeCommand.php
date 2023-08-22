c1ff7c3410634ddbe33737d38f54c777465178e3
Ilya <z-z-z88@list.ru>
2023-08-22 18:02:39 +0600

    registration fix_v2

--- commands/registration/RegistrationStepInvitationCodeCommand.php
+++ commands/registration/RegistrationStepInvitationCodeCommand.php
@@ -31,7 +31,7 @@ class RegistrationStepInvitationCodeCommand extends Command
 			'chat_id' => $telegram_id,
 		];
 
-        if($result['user']['invitation_code_id'] !== NULL) {
+        if(!empty($result['user']['invitation_code_id'])) {
             $options['text'] = __('You are already registered', $result['user']['language']);
             $this->telegram->sendMessage($options);
             return false;
