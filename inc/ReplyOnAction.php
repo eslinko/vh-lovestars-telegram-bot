93649624af1893e6a0d53a25315e44045f758196
Ilya <z-z-z88@list.ru>
2023-10-06 15:05:54 +0600

    test3 for ZEYA-187

--- inc/ReplyOnAction.php
+++ inc/ReplyOnAction.php
@@ -540,7 +540,11 @@ function registration_step_invitation_code($update, $telegram) {
     $telegram->sendMessage(['chat_id' => $update->getMessage()->chat->id, 'text' => json_encode($result)]);
 
 	if(isset($result['status'])==false OR $result['status'] !== 'success') {
-		$telegram->sendMessage(['chat_id' => $update->getMessage()->chat->id, 'text' => __($result['text'], $user['language']), 'reply_markup' => Keyboard::make([
+        if(isset($result['text']))
+            $text = $result['text'];
+        else
+            $text = 'Error! Try again later.';
+		$telegram->sendMessage(['chat_id' => $update->getMessage()->chat->id, 'text' => __($text, $user['language']), 'reply_markup' => Keyboard::make([
 			'inline_keyboard' =>  [
 				[
 					Keyboard::inlineButton([
