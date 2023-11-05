1c7e2d44535ac6c489394f94e279878566b8e2ed
Ilya <z-z-z88@list.ru>
2023-11-05 22:20:03 +0600

    ZEYA-237

--- commands/user/ReportAnIssueCommand.php
+++ commands/user/ReportAnIssueCommand.php
@@ -27,17 +27,19 @@ class ReportAnIssueCommand extends Command
         $update = $this->getUpdate();
         $telegram_id = $update->getMessage()->chat->id;
 
-        $user = user_is_verified($telegram_id);
+/*        $user = user_is_verified($telegram_id);
 
         if(!$user['status']) {
             return false;
-        }
+        }*/
+        $lcApi = new \LCAPPAPI();
+        $return_data = $lcApi->makeRequest('get-user-by-telegram-id', ['telegram_id' => $telegram_id, 'telegram_language_code' => $update->getMessage()->from->language_code]);
 
         $options = [
             'chat_id' => $telegram_id,
         ];
 
-        $options['text'] = __('If you ever run into a hiccup with our chatbot', $user['user']['language']);
+        $options['text'] = __('If you ever run into a hiccup with our chatbot', $return_data['user']['language']);
         $this->telegram->sendMessage($options);
         set_command_to_last_message($this->name, $telegram_id);
     }
