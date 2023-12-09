ef5b4245fdf867493f303ece78198736e00da6d2
Ilya <z-z-z88@list.ru>
2023-12-09 21:57:01 +0600

    notification settings

--- commands/user/NotificationSettings.php
+++ commands/user/NotificationSettings.php
@@ -41,7 +41,7 @@ class NotificationSettingsCommand extends Command
             $this->telegram->sendMessage(['chat_id' => $telegram_id, 'text' => __("Sorry, there was an error, please contact the administrator.", $user['user']['language'])]);
         if($data['status'] === 'success') {
             $button_command = [];
-            if($data['notify_connections'] == true) {
+            if($data['notify_connections'] == 1) {
                 $button_command['notify_connections'] = 'notification_settings__connections__0';
                 $button_command['notify_connections_title'] = __('Btn_notify_connections_is_on', $user['user']['language']);
             }
@@ -49,7 +49,7 @@ class NotificationSettingsCommand extends Command
                 $button_command['notify_connections'] = 'notification_settings__connections__1';
                 $button_command['notify_connections_title'] = __('Btn_notify_connections_is_off', $user['user']['language']);
             }
-            if($data['notify_matches'] == true) {
+            if($data['notify_matches'] == 1) {
                 $button_command['notify_matches'] = 'notification_settings__matches__0';
                 $button_command['notify_matches_title'] = __('Btn_notify_matches_is_on', $user['user']['language']);
             }
@@ -57,7 +57,7 @@ class NotificationSettingsCommand extends Command
                 $button_command['notify_matches'] = 'notification_settings__matches__1';
                 $button_command['notify_matches_title'] = __('Btn_notify_matches_is_off', $user['user']['language']);
             }
-            if($data['notify_invite_codes'] == true) {
+            if($data['notify_invite_codes'] == 1) {
                 $button_command['notify_invite_codes'] = 'notification_settings__invite_codes__0';
                 $button_command['notify_invite_codes_title'] = __('Btn_notify_invite_codes_is_on', $user['user']['language']);
             }
@@ -65,7 +65,7 @@ class NotificationSettingsCommand extends Command
                 $button_command['notify_invite_codes'] = 'notification_settings__invite_codes__1';
                 $button_command['notify_invite_codes_title'] = __('Btn_notify_invite_codes_is_off', $user['user']['language']);
             }
-            if($data['notify_ce_activity'] == true) {
+            if($data['notify_ce_activity'] == 1) {
                 $button_command['notify_ce_activity'] = 'notification_settings__ce_activity__0';
                 $button_command['notify_ce_activity_title'] = __('Btn_notify_ce_activity_is_on', $user['user']['language']);
             }
